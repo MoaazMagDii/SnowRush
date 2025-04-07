1450358822bf37f456bbeee0ceb6e5f4febbe9e3
MoaazMagDii <moaazmagdy79@gmail.com>
2025-04-08 01:47:23 +0200

    Connected the Database, Seeded it, tested GET controller

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,65 @@
+using API.Entities;
+
+namespace API.Data;
+
+public class DbInitializer
+{
+
+    public static void InitializeDb(WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+        context.Database.EnsureCreated();
+
+
+        if (context.Products.Any()) return;
+
+        var products = new List<Product>
+        {
+            new() {
+                Name = "Angular Speedster Board 2000",
+                Description =
+                    "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                Price = 20000,
+                PictureUrl = "/images/products/sb-ang1.png",
+                Brand = "Angular",
+                Type = "Boards",
+                Quantity = 100
+            },
+            new() {
+                Name = "Green Angular Board 3000",
+                Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                Price = 15000,
+                PictureUrl = "/images/products/sb-ang2.png",
+                Brand = "Angular",
+                Type = "Boards",
+                Quantity = 100
+            },
+            new() {
+                Name = "Core Board Speed Rush 3",
+                Description =
+                    "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                Price = 18000,
+                PictureUrl = "/images/products/sb-core1.png",
+                Brand = "NetCore",
+                Type = "Boards",
+                Quantity = 100
+            },
+            new() {
+                Name = "Net Core Super Board",
+                Description =
+                    "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                Price = 30000,
+                PictureUrl = "/images/products/sb-core2.png",
+                Brand = "NetCore",
+                Type = "Boards",
+                Quantity = 100
+            },
+        };
+
+
+        
+        context.Products.AddRange(products);
+        context.SaveChanges();
+    }
+}
